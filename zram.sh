73bf00a582dcd4a1bdadef2e7ec207789a39b5f1
Przemysław Łent <ple991@gmail.com>
2019-11-06 00:03:15 +0100

    Changed algorithm to lz4

--- zram.sh
+++ zram.sh
@@ -9,6 +9,7 @@ mem=$(( ($totalmem / $cores)* 1024 ))
 
 core=0
 while [ $core -lt $cores ]; do
+  echo lz4 > /sys/block/zram$core/comp_algorithm
   echo $mem > /sys/block/zram$core/disksize
   mkswap /dev/zram$core
   swapon -p 5 /dev/zram$core
